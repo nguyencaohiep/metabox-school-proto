d721f242bb0d2f4cabc83a8475ff14b2945a8b80
Nguyễn Cao Hiệp <apolohwork@gmail.com>
2024-05-16 14:49:43 +0700

    check ethnic student

--- golang/student/student_grpc.pb.go
+++ golang/student/student_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion7
 const (
 	StudentService_StudentById_FullMethodName                   = "/mex.school.v1.StudentService/StudentById"
 	StudentService_StudentUpdateIsHaveAspiration_FullMethodName = "/mex.school.v1.StudentService/StudentUpdateIsHaveAspiration"
+	StudentService_CheckEthnicStudent_FullMethodName            = "/mex.school.v1.StudentService/CheckEthnicStudent"
 )
 
 // StudentServiceClient is the client API for StudentService service.
@@ -29,6 +30,7 @@ const (
 type StudentServiceClient interface {
 	StudentById(ctx context.Context, in *StudentByIdRequest, opts ...grpc.CallOption) (*Student, error)
 	StudentUpdateIsHaveAspiration(ctx context.Context, in *StudentUpdateIsHaveAspirationRequest, opts ...grpc.CallOption) (*StudentUpdateIsHaveAspirationRespone, error)
+	CheckEthnicStudent(ctx context.Context, in *CheckEthnicStudentRequest, opts ...grpc.CallOption) (*CheckEthnicStudentRespone, error)
 }
 
 type studentServiceClient struct {
@@ -57,12 +59,22 @@ func (c *studentServiceClient) StudentUpdateIsHaveAspiration(ctx context.Context
 	return out, nil
 }
 
+func (c *studentServiceClient) CheckEthnicStudent(ctx context.Context, in *CheckEthnicStudentRequest, opts ...grpc.CallOption) (*CheckEthnicStudentRespone, error) {
+	out := new(CheckEthnicStudentRespone)
+	err := c.cc.Invoke(ctx, StudentService_CheckEthnicStudent_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // StudentServiceServer is the server API for StudentService service.
 // All implementations must embed UnimplementedStudentServiceServer
 // for forward compatibility
 type StudentServiceServer interface {
 	StudentById(context.Context, *StudentByIdRequest) (*Student, error)
 	StudentUpdateIsHaveAspiration(context.Context, *StudentUpdateIsHaveAspirationRequest) (*StudentUpdateIsHaveAspirationRespone, error)
+	CheckEthnicStudent(context.Context, *CheckEthnicStudentRequest) (*CheckEthnicStudentRespone, error)
 	mustEmbedUnimplementedStudentServiceServer()
 }
 
@@ -75,6 +87,9 @@ func (UnimplementedStudentServiceServer) StudentById(context.Context, *StudentBy
 }
 func (UnimplementedStudentServiceServer) StudentUpdateIsHaveAspiration(context.Context, *StudentUpdateIsHaveAspirationRequest) (*StudentUpdateIsHaveAspirationRespone, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method StudentUpdateIsHaveAspiration not implemented")
+}
+func (UnimplementedStudentServiceServer) CheckEthnicStudent(context.Context, *CheckEthnicStudentRequest) (*CheckEthnicStudentRespone, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckEthnicStudent not implemented")
 }
 func (UnimplementedStudentServiceServer) mustEmbedUnimplementedStudentServiceServer() {}
 
@@ -125,6 +140,24 @@ func _StudentService_StudentUpdateIsHaveAspiration_Handler(srv interface{}, ctx 
 	return interceptor(ctx, in, info, handler)
 }
 
+func _StudentService_CheckEthnicStudent_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CheckEthnicStudentRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(StudentServiceServer).CheckEthnicStudent(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: StudentService_CheckEthnicStudent_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(StudentServiceServer).CheckEthnicStudent(ctx, req.(*CheckEthnicStudentRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // StudentService_ServiceDesc is the grpc.ServiceDesc for StudentService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -139,6 +172,10 @@ var StudentService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "StudentUpdateIsHaveAspiration",
 			Handler:    _StudentService_StudentUpdateIsHaveAspiration_Handler,
+		},
+		{
+			MethodName: "CheckEthnicStudent",
+			Handler:    _StudentService_CheckEthnicStudent_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
